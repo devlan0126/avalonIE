98d1aebf692846209e1a4cdd3f26641ba03473ed
devlan0126 <wyang0126@163.com>
2024-04-12 11:59:04 +0800

    update

--- app/source/modules/mdcAdrg/index.js
+++ app/source/modules/mdcAdrg/index.js
@@ -1,39 +1,116 @@
+/*
+ * @Date: 2024-03-19 21:01:59
+ * @LastEditors: devlan0126 wyang0126@163.com
+ * @LastEditTime: 2024-04-12 11:31:41
+ * @FilePath: \avalonIE\app\source\modules\mdcAdrg\index.js
+ * @Description: 文档描述
+ */
 avalon.component("ms-mdcAdrg", {
     template: require("./template.html"),
     defaults: {
         show: false,
-        list: [],
-        columns: [
-            {
-                label: "主诊断编码",
-                prop: "mainDiagCode",
-            },
-            {
-                label: "主诊断名称",
-                prop: "mainDiagName",
-            },
-            {
-                label: "主手术编码",
-                prop: "mainOprnCode",
-            },
-            {
-                label: "主手术名称",
-                prop: "mainOprnName",
-            },
-            {
-                label: "DRG编码",
-                prop: "drgCode",
-            },
-            {
-                label: "DRG名称",
-                prop: "drgName",
-            },
-        ],
-
+        diagList: [],
+        diagTotal: 0,
+        diagCurrentPage: 1,
+        operList: [],
+        operTotal: 0,
+        operCurrentPage: 1,
+        diagTimer: null,
+        operTimer: null,
+        searchDiagValue: "",
+        searchOperValue: "",
+        onDiagChange: function ($event) {
+            var that = this
+            clearTimeout(that.diagTimer);
+            that.diagTimer = setTimeout(function () {
+                that.resetDiagTableData()
+                var srcElement = $event.srcElement;
+                if (srcElement) {
+                    var value = srcElement.value;
+                    that.searchDiagValue = value
+                    if (value) {
+                        that.diagCurrentPage = 1;
+                        that.requestDiagList()
+                    }
+                }
+            }, 1000)
+        },
+        onDiagPageClick: function ($event, page) {
+            this.diagCurrentPage = page;
+            this.requestDiagList()
+        },
+        requestDiagList: function () {
+            var that = this
+            $.ajax({
+                url: '/hprs/sim/diagQuery',
+                type: 'GET',
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                dataType: 'json',
+                data: 'ver=CHS628&pageNum=' + that.diagCurrentPage + '&pageSize=10&data=' + that.searchDiagValue,
+                success: function (res) {
+                    if (res.code === 200) {
+                        that.diagList = res.data
+                        that.diagTotal = res.data.length;
+                    }
+                }
+            });
+        },
+        resetDiagTableData: function () {
+            this.diagList = []
+            this.diagTotal = 0
+            this.diagCurrentPage = 1
+            this.searchDiagValue = ""
+        },
+        onOperChange: function ($event) {
+            var that = this
+            clearTimeout(that.operTimer);
+            that.operTimer = setTimeout(function () {
+                that.resetOperTableData()
+                var srcElement = $event.srcElement;
+                if (srcElement) {
+                    var value = srcElement.value;
+                    that.searchOperValue = value
+                    if (value) {
+                        that.operCurrentPage = 1;
+                        that.requestOperList()
+                    }
+                }
+            }, 1000)
+        },
+        onOperPageClick: function ($event, page) {
+            this.operCurrentPage = page;
+            this.requestOperList()
+        },
+        requestOperList: function () {
+            var that = this
+            $.ajax({
+                url: '/hprs/sim/operQuery',
+                type: 'GET',
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                dataType: 'json',
+                data: 'ver=CHS628&pageNum=' + that.operCurrentPage + '&pageSize=10&data=' + that.searchOperValue,
+                success: function (res) {
+                    if (res.code === 200) {
+                        that.operList = res.data;
+                        that.operTotal = res.data.length;
+                    }
+                }
+            });
+        },
+        resetOperTableData: function () {
+            this.operList = []
+            this.operTotal = 0
+            this.operCurrentPage = 1
+            this.searchOperValue = ""
+        },
         onInit: function () {
-
             var that = this;
-            // query the list data
             setTimeout(function () {
                 that.list = [
                     { mainDiagCode: "item1", mainDiagName: "mainDiagName", mainOprnCode: 'mainOprnCode', mainOprnName: 'mainOprnName', drgCode: 'drgCode', drgName: 'drgName' },
@@ -61,7 +138,7 @@ avalon.component("ms-mdcAdrg", {
 
 function resetListHeight() {
     var $tab = $('.ui-tabs-panel')
-    var h = $tab.height() - 50;
-    $('.mdc-fzzl-tab__list').css('height', h + 'px');
+    var h = $tab.height() - 50 - 45;
+    $('.mdc-mdcAdrg-tab__list').css('height', h + 'px');
 
 }
