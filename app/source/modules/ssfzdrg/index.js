d09540e87f78d40d881fd6af51c9de492cc48246
devlan0126 <wyang0126@163.com>
2024-04-16 15:48:57 +0800

    update

--- app/source/modules/ssfzdrg/index.js
+++ app/source/modules/ssfzdrg/index.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2024-04-10 14:53:21
  * @LastEditors: devlan0126 wyang0126@163.com
- * @LastEditTime: 2024-04-12 16:52:46
+ * @LastEditTime: 2024-04-16 15:32:18
  * @FilePath: \avalonIE\app\source\modules\ssfzdrg\index.js
  * @Description: 文档描述
  */
@@ -13,7 +13,31 @@ avalon.component("ms-ssfzdrg", {
         list: [],
         source: [],
         zIndex: 1,
-        timer: null,
+        timer: {},
+        groupInfo: {
+            mccOutcome: '',
+            ccOutcome: '',
+            icd10Adrg: '',
+            icd9Adrg: '',
+            drgInfo: {
+                drgCode: '',
+                drgName: ''
+            }
+        },
+        setlInfo: {
+            grpAvgAmt: "",// 病组均费
+            score: "",// 基准点数
+            feeRatio: 0, // 费用比例
+            stdDiffAmt: 0, // 标准差额
+            adjustmentFactor: 0, // 调整系数
+            grpStdAmt: 0,// 费用标准
+            caseType: '-',// 病例类型
+            yhds: "",// 预测点数
+            predictAmt: '0',// 预测费用
+            predictProfit: 0,// 预测盈亏
+            medFeeAmt: 0,// 总费用
+        },
+        warnMsgList: [],
         onClick: function ($event) {
         },
         onChange: function ($event) {
--- app/view/index/index.js
+++ app/view/index/index.js
@@ -32,11 +32,7 @@ require('../../source/modules/dfcs/index')
 
 var vm = avalon.define({
   $id: "native",
-  name: "司徒正美",// 姓名
-  hospitalNumber: '2983930',//住院号
-  serialNumber: '18293943845959000',//流水号
-  inHospitalTime: '2024年3月7日 19点24分',//入院时间
-  array: [11, 22, 33],
+  serialNo: '',
   mainDiagnose: 'J18.903,重症肺炎',
   mainOperation: '31.1x00x005,暂时性气管切开术',
   clrWay: '40',
@@ -61,16 +57,23 @@ var vm = avalon.define({
   },
   isLarge: false,
   dataForm: {
-    name: '司徒正美',
-    medFeeAmt: 2273.22,// 医疗费用
-    actIptDays: 12,// 住院天数
-    age: 11,
-    insuType: '01',
-    setlMon: '202404',
-    fixmedinsCode: 'H118282',
-    ventUsedHCnt: '11',
-    sex: "1"
+    name: '-',
+    medFeeAmt: 0,// 医疗费用
+    actIptDays: 0,// 住院天数
+    age: 0,
+    insuType: '',
+    setlMon: '',
+    fixmedinsCode: '',
+    ventUsedHCnt: '',
+    sex: "1",
+    serialNo: '',
+    hospitalNo: '',
+    inHospitalTime: '',
   },
+  pageTitle: '医院DRG/DIP数据精细化治理',
+  groupInfo: {},
+  setlInfo: {},
+  warnMsgList: [],
   tabClick: function ($event, index) {
     var visible = this.tabConfig['tabVisible' + index]
     this.tabConfig.tabVisible1 = false
@@ -130,23 +133,89 @@ var vm = avalon.define({
   },
   onSubmit: function () {
     var that = this
-    $.ajax({
-      url: '/hprs/sim/grpsetl',
-      type: 'POST',
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      dataType: 'json',
-      success: function (res) {
-        if (res.code === 200) {
-          that.groupInfo = {
-            ...data,
-            medFeeAmt: that.dataForm.medFeeAmt,
-          };
-        }
+    // $.ajax({
+    //   url: '/hprs/sim/grpsetl',
+    //   type: 'POST',
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    //   dataType: 'json',
+    //   success: function (res) {
+    //     if (res.code === 200) {
+    //       that.groupInfo = {
+    //         ...data,
+    //         medFeeAmt: that.dataForm.medFeeAmt,
+    //       };
+    //     }
+    //   }
+    // });
+    var res = {
+      "msg": "成功",
+      "code": 200,
+      "data": {
+        "gid": "c5eabfe6-0db9-407b-9101-f1468952190a",
+        "code": 200,
+        "fine": true,
+        "message": null,
+        "setlId": null,
+        "fixmedinsCode": "H13068200954",
+        "clrOptins": "130682",
+        "setlMon": "202404",
+        "insuType": "390",
+        "actIptDays": 12,
+        "setlAmt": 66789,
+        "drgInfo": {
+          "fine": true,
+          "message": "",
+          "drgCode": "ES35",
+          "drgName": "呼吸系统感染/炎症，不伴并发症或合并症",
+          "adrg": "ES3",
+          "adrgName": "呼吸系统感染/炎症",
+          "ccmcc": "NO",
+          "cod": "100",
+          "codd": "内科组",
+          "mdc": "MDCE",
+          "mdcName": "呼吸系统疾病及功能障碍",
+          "ccList": "K91.833,胃肠吻合口水肿|E11.600x015+M14.2*,2型糖尿病性肩关节周围炎",
+          "mccList": "K91.833,胃肠吻合口水肿|E11.600x015+M14.3"
+        },
+        "dipInfo": null,
+        "setlInfo": {
+          "fine": true,
+          "message": null,
+          "caseType": "高限病例",
+          "feeRatio": null,
+          "grpStdAmt": 0.00,
+          "stdDiffAmt": 66789.00,
+          "dayDiff": null,
+          "predictProfit": -63848.56,
+          "predictAmt": 2940.44,
+          "predictPointValue": 0.4527,
+          "pointValue": 6495.3400,
+          "basePointScore": 0.4527,
+          "grpAvgAmt": 0.00,
+          "adjustmentFactor": 1.0000,
+          "stdGrpDays": 0.00,
+          "lowAmt": 599.85,
+          "highAmt": 4498.87,
+          "msg": null
+        },
+        "mccOutcome": "K91.833",
+        "ccOutcome": "K91.833,胃肠吻合口水肿|E11.600x015+M14.2*,2型糖尿病性肩关节周围炎",
+        "icd10Adrg": "ES3",
+        "icd9Adrg": "",
+        "warnMsgList": ['主诊断不满足分组要求，请修订主诊断；', '主诊断与主手术不匹配，入歧义(QY)病组，建议核查修订主诊断或主手术；', '高倍率病例，费用较高，需合理控费或核对诊疗信息准确性;']
       }
-    });
+    }
+
+    res.data.medFeeAmt = that.dataForm.medFeeAmt;
+    that.groupInfo = res.data;
+    if (res.code === 200) {
+      that.setlInfo = encodeGroupInfo(res.data);
+      that.warnMsgList = res.data.warnMsgList || [];
+    }
+    console.log('groupInfo>', that.groupInfo)
   },
   onRest: function () {
     window.location.reload()
@@ -166,7 +235,6 @@ var vm = avalon.define({
 });
 
 vm.$watch('onReady', function (v) {
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', JSON.stringify([{ a: "诶" }, { b: "比" }]));
   if (window.clientWidth < 1367) {
     // 1366*768
     this.pageClass = 'w-1366'
@@ -182,6 +250,29 @@ vm.$watch('onReady', function (v) {
   }
 
   var that = this
+  that.clrWay = getUrlParam('clrWay') || '40';
+  that.serialNo = getUrlParam('serialNo') || '18293943845959000'
+
+  getPageInfo(that.serialNo, function (data) {
+    that.dataForm = {
+      name: '经典可乐',
+      medFeeAmt: 2273.22,// 医疗费用
+      actIptDays: 12,// 住院天数
+      age: 23,
+      insuType: '01',
+      setlMon: '202404',
+      fixmedinsCode: 'H118282',
+      ventUsedHCnt: '11',
+      sex: "1",
+      serialNo: that.serialNo,
+      hospitalNo: '2983930',
+      inHospitalTime: '2024年3月7日 19点24分',
+    }
+    that.pageTitle = '医院DRG/DIP数据精细化治理'
+
+    that.onSubmit()
+  })
+
   getSysConfig(function (data) {
     console.log('getSysConfig>', data)
     that.showTabs = {
@@ -194,7 +285,6 @@ vm.$watch('onReady', function (v) {
       clinPathEnabled: data.clinPathEnabled == '1', // 是否开启临床路径指南查
     }
   })
-  this.clrWay = getUrlParam('clrWay') || '40'
   window.lonsOperations = []
   window.lonsDiagnoses = []
   getOperationList()
@@ -291,8 +381,60 @@ function getSysConfig(callback) {
   });
 }
 
+function getPageInfo(serialNo, callback) {
+  // $.ajax({
+  //   url: '/hprs/api/pop/getSysConfig',
+  //   type: 'GET',
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   dataType: 'json',
+  //   success: function (res) {
+  //     if (res.code === 200) {
+  //       callback(res.data)
+  //     }
+  //   }
+  // });
+  callback()
+
+}
+
 function getUrlParam(name) {
   var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
   var r = window.location.search.substr(1).match(reg);  //匹配目标参数
   if (r != null) return unescape(r[2]); return null; //返回参数值
+}
+
+
+function encodeGroupInfo(groupInfo) {
+  if (groupInfo && groupInfo.setlInfo) {
+    var setlInfo = groupInfo.setlInfo;
+    return {
+      grpAvgAmt: setlInfo.grpAvgAmt,// 病组均费
+      score: setlInfo.score,// 基准点数
+      feeRatio: setlInfo.feeRatio, // 费用比例
+      stdDiffAmt: setlInfo.stdDiffAmt, // 标准差额
+      adjustmentFactor: setlInfo.adjustmentFactor, // 调整系数
+      grpStdAmt: setlInfo.grpStdAmt,// 费用标准
+      caseType: setlInfo.caseType,// 病例类型
+      yhds: setlInfo.yhds,// 预测点数
+      predictAmt: setlInfo.predictAmt,// 预测费用
+      predictProfit: setlInfo.predictProfit,// 预测盈亏
+      medFeeAmt: groupInfo.medFeeAmt,// 总费用
+    };
+  }
+  return {
+    grpAvgAmt: "",// 病组均费
+    score: "",// 基准点数
+    feeRatio: 0, // 费用比例
+    stdDiffAmt: 0, // 标准差额
+    adjustmentFactor: 0, // 调整系数
+    grpStdAmt: 0,// 费用标准
+    caseType: '-',// 病例类型
+    yhds: "",// 预测点数
+    predictAmt: '0',// 预测费用
+    predictProfit: 0,// 预测盈亏
+    medFeeAmt: 0,// 总费用
+  };
 }
