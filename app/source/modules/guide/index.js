6ccfbbf70fd83a9992af7c24c9927065ae1faee5
devlan0126 <wyang0126@163.com>
2024-04-25 11:13:31 +0800

    update

--- app/source/modules/guide/index.js
+++ app/source/modules/guide/index.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2024-04-24 14:16:38
  * @LastEditors: devlan0126 wyang0126@163.com
- * @LastEditTime: 2024-04-25 10:05:01
+ * @LastEditTime: 2024-04-25 10:55:25
  * @FilePath: \avalonIE\app\source\modules\guide\index.js
  * @Description: 文档描述
  */
@@ -32,6 +32,8 @@ avalon.component("ms-guide", {
                 if (that.list.length > 0) {
                     var pngFiles = that.list[0].pngFiles
                     var folderName = that.list[0].folderName
+                    $('.bok-select').find('.placeholder').text(folderName);
+
                     pdfUrl = '/hprs/sim/clinic/download/' + folderName + '/' + that.list[0].pdfFiles[0]
                     if (pngFiles && pngFiles.length > 0) {
                         for (var index = 0; index < pngFiles.length; index++) {
@@ -58,10 +60,50 @@ avalon.component("ms-guide", {
             this.requestList()
         },
         onChange: function ($event) {
-            console.log('select change');
+            console.log('select change', $event.target.value);
+            var index = $event.target.value;
+            var item = this.list[index]
+            window.pdfUrl = '/hprs/sim/clinic/download/' + item.folderName + '/' + item.pdfFiles[0]
+
+            var pngFiles = item.pngFiles
+            var imgList = []
+            if (pngFiles && pngFiles.length > 0) {
+                for (var index = 0; index < pngFiles.length; index++) {
+                    imgList.push('/hprs/sim/clinic/img/' + item.folderName + '/' + pngFiles[index])
+                }
+            }
+            insertImgs(imgList)
+            return this.guideEmit(imgList)
         },
         onImgClick: function ($event) {
             return this.previewImg('')
+        },
+        onSelectClick: function ($event) {
+            var parent = $('.bok-select')
+            if (!parent.hasClass('is-open')) {
+                parent.addClass('is-open');
+                $('.select.is-open').not(parent).removeClass('is-open');
+            } else {
+                parent.removeClass('is-open');
+            }
+            $event.stopPropagation();
+        },
+        onSelectItemClick: function ($event, index) {
+            var parent = $('.bok-select')
+
+            var item = this.list[index]
+            window.pdfUrl = '/hprs/sim/clinic/download/' + item.folderName + '/' + item.pdfFiles[0]
+
+            var pngFiles = item.pngFiles
+            var imgList = []
+            if (pngFiles && pngFiles.length > 0) {
+                for (var index = 0; index < pngFiles.length; index++) {
+                    imgList.push('/hprs/sim/clinic/img/' + item.folderName + '/' + pngFiles[index])
+                }
+            }
+            insertImgs(imgList)
+            parent.removeClass('is-open').find('.placeholder').text(item.folderName);
+            return this.guideEmit(imgList)
         }
     },
 });
@@ -74,3 +116,9 @@ function insertImgs(imgList) {
     }
     $('#guideImgContent').html(htmlStr);
 }
+
+$(function () {
+    $('body').on('click', function () {
+        $('.bok-select.is-open').removeClass('is-open');
+    });
+})
